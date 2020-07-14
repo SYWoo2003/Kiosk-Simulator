116879c271cc8355cadec6ef947487d5391618e3
Woo-sang yun <wsy04018@naver.com>
2020-07-14 16:06:38 +0900

    Update MenuButton.js

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -25,7 +25,7 @@ const MenuButton = ({ img, desc }) => {
     }
     @media screen and (max-width: 800px) {
             width: 4.5rem;
-            height: 4.5rem;
+            height: 5.5rem;
             border-radius: 0.5rem;
             padding: 0;
     }
